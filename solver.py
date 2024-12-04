2def40125194623c4bab097ba7785144639f244b
fiona-mdt <mordenti.fiona@gmail.com>
2024-12-04 17:41:09 +0000

    ajout fonction generate_valid_words

--- solver.py
+++ solver.py
@@ -1,10 +1,33 @@
 import math
 from copy import copy
 
+def generate_valid_words(possible_words: list[str], letters_in_secret: list[tuple[str, int]], letters_not_in_secret: list[str]) -> list[str]:
+    """
+    Génère une liste de mots valides à partir de la liste de mots potentiels, 
+    en fonction des lettres déjà trouvées et des lettres exclues.
+    """
+    mots_valides = []
+    
+    lettres_exclues = set(letters_not_in_secret) # Création d'un ensemble pour letters_not_in_secret pour vérifier rapidement les lettres
 
-def generate_valid_words(possible_words, letters_in_secret, letters_not_in_secret):
-    return []
-
+    for mot in possible_words:
+        est_valide = True
+        
+        for lettre in lettres_exclues: # Vérification du mot pour voir s'il ne contient pas de lettres exclues
+            if lettre in mot:
+                est_valide = False
+                break
+        
+        if est_valide: # Vérification de la position des lettres
+            for lettre, position in letters_in_secret:
+                if mot[position] != lettre:
+                    est_valide = False
+                    break
+        
+        if est_valide:
+            mots_valides.append(mot)
+    
+    return mots_valides
 
 
 
